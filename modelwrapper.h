b89f998c08c8d0b90dde49250e19e36883c2827d
Plavetic Andrija (BME) <a.plavetic@maastrichtuniversity.nl>
2024-05-08 17:35:29 +0200

    Make ModelWrapper a QObject class

--- modelwrapper.h
+++ modelwrapper.h
@@ -1,12 +1,17 @@
 #ifndef MODELWRAPPER_H
 #define MODELWRAPPER_H
 #include <QVector>
+#include <QObject>
 #include "Core.hpp"
 
-class ModelWrapper : public CAcore::Core {
+class ModelWrapper : public QObject, public CAcore::Core {
+        Q_OBJECT
 
 public:
-    ModelWrapper() : CAcore::Core(new CAcore::Factory) {
+    ModelWrapper(QObject *parent = Q_NULLPTR) :
+        QObject(parent),
+        CAcore::Core(new CAcore::Factory)
+    {
         build("VanOsta2022", "forward_euler");
         };
 
