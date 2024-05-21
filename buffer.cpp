3df5d32bbc133524637b46128a3e12e69cc17b74
Plavetic Andrija (BME) <a.plavetic@maastrichtuniversity.nl>
2024-05-21 16:49:10 +0200

    Add check for empty vector in buffer read

--- buffer.cpp
+++ buffer.cpp
@@ -11,38 +11,55 @@ QVector<double> Buffer::take(const QString& string, double dt)
 {
     int count = 0;
     QVector<double>& vec = data[string];
-    for (int i = 0; i < data["t"].size(); i++) {
-        if (qAbs(data["t"][i] - data["t"][0]) <= dt) {
-            ++count;
+    if (!vec.empty())
+    {
+        for (int i = 0; i < data["t"].size(); i++)
+        {
+            if (qAbs(data["t"][i] - data["t"][0]) <= dt)
+            {
+                ++count;
+            }
+            else break;
         }
-        else break;
-    }
 
-    QVector<double> out;
-    out.reserve(count);
-    std::move(vec.begin(),
+        QVector<double> out;
+        out.reserve(count);
+        std::move(vec.begin(),
               vec.begin() + count,
               std::back_inserter(out));
 
-    // Erase the moved elements from originalVector
-    vec.erase(vec.begin(), vec.begin() + count);
-    return out;
+        vec.erase(vec.begin(), vec.begin() + count);
+        return out;
+    }
+    else
+    {
+        throw std::range_error(string.toStdString() + " buffer empty!");
+    }
 }
 
 QVector<double> Buffer::get(const QString& string, double dt)
 {
     int count = 0;
     QVector<double>& vec = data[string];
-    for (int i = 0; i < data["t"].size(); i++) {
-        if (qAbs(data["t"][i] - data["t"][0]) <= dt) {
-            ++count;
+    if (!vec.empty())
+    {
+        for (int i = 0; i < data["t"].size(); i++)
+        {
+            if (qAbs(data["t"][i] - data["t"][0]) <= dt)
+            {
+                ++count;
+            }
+            else break;
         }
-        else break;
-    }
 
-    QVector<double> out;
-    out.reserve(count);
-    std::copy(vec.begin(), vec.begin() + count, std::back_inserter(out));
-    return out;
+        QVector<double> out;
+        out.reserve(count);
+        std::copy(vec.begin(), vec.begin() + count, std::back_inserter(out));
+        return out;
+    }
+    else
+    {
+        throw std::range_error(string.toStdString() + " buffer empty!");
+    }
 
 }
