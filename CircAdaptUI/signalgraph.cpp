db829b1be6af41109d35af01d03085dce3a77ad0
Plavetic Andrija (BME) <a.plavetic@maastrichtuniversity.nl>
2024-06-05 13:47:21 +0200

    Change folder structure

--- CircAdaptUI/signalgraph.cpp
+++ CircAdaptUI/signalgraph.cpp
@@ -8,7 +8,6 @@ SignalGraph::SignalGraph(QWidget* parent) :
     xAxis->setLabelFont(QFont("Mononoki", 14, QFont::Bold));
     yAxis->setLabel("Pressure [mmHg]");
     yAxis->setLabelFont(QFont("Mononoki", 14, QFont::Bold));
-    xAxis->setRangeUpper(1.5);
     yAxis->grid()->setZeroLinePen(QPen(QColor(0,0,0), 1, Qt::DashLine, Qt::RoundCap, Qt::RoundJoin));
     axisRect()->setRangeDrag(Qt::Vertical);
 }
