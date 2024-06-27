e0091b9ab148ff5405159404323a03e341c993b7
Plavetic Andrija (BME) <a.plavetic@maastrichtuniversity.nl>
2024-06-27 16:24:04 +0200

    Load setings before everything else

--- CircAdaptUI/main.cpp
+++ CircAdaptUI/main.cpp
@@ -1,8 +1,10 @@
 #include <QApplication>
 #include "circadaptui.h"
+#include "settings.h"
 
 int main(int argc, char *argv[])
 {
+    Settings::instance().load("config.json");
     CircAdaptUI circadaptui(argc, argv);
 
     return circadaptui.exec();
