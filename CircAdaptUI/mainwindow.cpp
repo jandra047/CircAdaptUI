5a38facb556ac39abb0fabd265a49bafc9b11cf2
Plavetic Andrija (BME) <a.plavetic@maastrichtuniversity.nl>
2024-08-27 15:00:51 +0200

    Change names of loop signals

--- CircAdaptUI/mainwindow.cpp
+++ CircAdaptUI/mainwindow.cpp
@@ -28,9 +28,9 @@ MainWindow::MainWindow(ModelWrapper& mw, Buffer& buffer, QWidget *parent)
     auto pvMenu = ui->pvGraph->buildMenu(ui->pvGraph);
     ui->pvGraph->setContextMenu(pvMenu);
     QObject::connect(pvMenu.second, &QActionGroup::triggered, this, [=](QAction* action) { ui->pvGraph->showSignal(action); });
-    LoopSignal* sig3 = new LoopSignal(ui->ssGraph->xAxis, ui->ssGraph->yAxis, "LV", "Sf_Lv", "Ef_Lv", QColor(227, 26, 28));
-    LoopSignal* sig4 = new LoopSignal(ui->ssGraph->xAxis, ui->ssGraph->yAxis, "Septum", "Sf_Sv", "Ef_Sv", QColor(0,0,0));
-    LoopSignal* sig5 = new LoopSignal(ui->ssGraph->xAxis, ui->ssGraph->yAxis, "RV", "Sf_Rv", "Ef_Rv", QColor(31, 120, 180));
+    LoopSignal* sig3 = new LoopSignal(ui->ssGraph->xAxis, ui->ssGraph->yAxis, "Left ventricular free wall", "Sf_Lv", "Ef_Lv", QColor(227, 26, 28));
+    LoopSignal* sig4 = new LoopSignal(ui->ssGraph->xAxis, ui->ssGraph->yAxis, "Septal wall", "Sf_Sv", "Ef_Sv", QColor(0,0,0));
+    LoopSignal* sig5 = new LoopSignal(ui->ssGraph->xAxis, ui->ssGraph->yAxis, "Right ventricular free wall", "Sf_Rv", "Ef_Rv", QColor(31, 120, 180));
     ui->ssGraph->addSignal(sig3);
     ui->ssGraph->addSignal(sig4);
     ui->ssGraph->addSignal(sig5);
