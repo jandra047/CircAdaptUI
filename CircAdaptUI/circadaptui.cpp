990e16cfead0f24e095388883222dfad79d3954e
Plavetic Andrija (BME) <a.plavetic@maastrichtuniversity.nl>
2024-10-23 11:54:05 +0200

    Move realtime slot to separate thread

--- CircAdaptUI/circadaptui.cpp
+++ CircAdaptUI/circadaptui.cpp
@@ -12,19 +12,23 @@ CircAdaptUI::CircAdaptUI(int &argc, char **argv):
     Settings::instance().load(qApp->applicationDirPath().append("/config.json"));
     QThread::currentThread()->setPriority(QThread::HighestPriority);
     m_mainwindow = new MainWindow(m_mw, m_buffer);
+    m_thread = new MyThread(m_mainwindow, this);
 
     connect(m_mainwindow, &MainWindow::updateDone, &m_mw, &ModelWrapper::run_steps);
     connect(&m_mw, &ModelWrapper::setup_done, m_mainwindow, &MainWindow::displayReference);
-    connect(m_timer, &QTimer::timeout, this, &CircAdaptUI::realtimeSlot);
+    // connect(m_timer, &QTimer::timeout, this, &CircAdaptUI::realtimeSlot);
     connect(m_mainwindow, &MainWindow::togglePlay, this, &CircAdaptUI::togglePlay);
     connect(m_mainwindow, &MainWindow::resetSignal, this, &CircAdaptUI::reset);
 
     fps = Settings::instance().fps();
 
-    connect(&m_mw, &ModelWrapper::setup_done, this, [=]() { m_timer->start(0); });
+    m_thread->start();
+    connect(&m_mw, &ModelWrapper::setup_done, this, [=]() { m_thread->timer->start(0); });
     m_mw.setup();
     m_mainwindow->show();
     m_mainwindow->autoscaleAll();
+
+    // m_thread.timer->start(0);
 
 }
 
--- CircAdaptUI/circadaptui.h
+++ CircAdaptUI/circadaptui.h
@@ -5,8 +5,54 @@
 #include <QApplication>
 #include "mainwindow.h"
 
+class MyThread : public QThread
+{
+    Q_OBJECT
+public:
+    MyThread() {};
+    MyThread(MainWindow* mw, QObject* parent = Q_NULLPTR):
+        QThread(parent),
+        m_mainwindow(mw)
+    {
+        connect(timer, &QTimer::timeout, this, &MyThread::realtimeSlot);
+    };
+    QTimer* timer = new QTimer(this);
+    MainWindow* m_mainwindow;
+    double fps = 60;
+public slots:
+    void realtimeSlot()
+    {
+
+        static QTime timeStart = QTime::currentTime();
+        static double trueFPS = 1000;
+        double timeElapsed = timeStart.msecsTo(QTime::currentTime())/1000.0;
+        static double timeLastUpdate = 0;
+        static double timeLastFps;
+        static int frameCount;
+
+        if (timeElapsed - timeLastUpdate > 1/fps)
+        {
+            double currfps = 1/(timeElapsed - timeLastUpdate);
+            m_mainwindow->updateGraphs(
+                (fps < trueFPS) ? 1/currfps : 1/trueFPS
+                );
+            timeLastUpdate = timeElapsed;
+        }
+        ++frameCount;
+
+        if (timeElapsed - timeLastFps > 0.01)
+        {
+            trueFPS = frameCount/(timeElapsed-timeLastFps);
+            timeLastFps = timeElapsed;
+            frameCount = 0;
+        }
+
+    }
+};
+
 class CircAdaptUI : public QApplication
 {
+    Q_OBJECT
 public:
     CircAdaptUI(int &argc, char **argv);
     ~CircAdaptUI();
@@ -16,6 +62,8 @@ private:
     QTimer *m_timer = new QTimer(this);
     Buffer m_buffer{};
     ModelWrapper m_mw;
+
+    MyThread* m_thread;
 
     double fps;
 
